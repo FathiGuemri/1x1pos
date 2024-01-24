4d8f9a9e6008a117537fa0daaeffde6cb17f9eef
FathiGuemri <zlatan.guemri@gmail.com>
2024-01-24 07:43:30 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '1x1pos',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
